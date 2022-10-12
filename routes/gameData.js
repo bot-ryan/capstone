51682006d8197b9a1bc46d4184cded41cb5110cc
Vivian Loo <vivian.lvhw@gmail.com>
2022-10-12 08:25:07 +0800

    add updateGamePlayers

--- routes/gameData.js
+++ routes/gameData.js
@@ -1,5 +1,5 @@
 import express from 'express';
-import { getGame,getGames,createGame,deleteGame,updateGame } from '../controllers/gameDataController.js';
+import { getGame,getGames,createGame,deleteGame,updateGame, updateGamePlayers } from '../controllers/gameDataController.js';
 
 const router = express.Router();
 
@@ -8,5 +8,6 @@ router.get('/:id', getGame);
 router.post('/create', createGame);
 router.delete('/delete', deleteGame);
 router.patch('/:id', updateGame);
+router.patch('/:id/:playerId', updateGamePlayers);
 
 export default router;
